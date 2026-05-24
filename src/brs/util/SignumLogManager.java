a1732982803e30be5428f0ca69fa433a8ab21cb2
BalazsGit <juhasz-nagyb@freemail.hu>
2026-05-24 16:36:16 +0200

    refactor: make SignumLogManager public and standardize formatting
    - Updated the visibility of the SignumLogManager class from package-private to public. This allows the custom log manager to be correctly instantiated and referenced by the JVM during the logging initialization phase, especially when configured via system properties.
    - Standardized the code indentation and formatting throughout the file to match the project's coding style (4-space indentation).
    - Maintained the existing logic that prevents log handlers from being reset prematurely during the application's shutdown sequence.

--- src/brs/util/SignumLogManager.java
+++ src/brs/util/SignumLogManager.java
@@ -8,57 +8,57 @@ import java.util.logging.LogManager;
 /**
  * Java LogManager extension for use with Signum
  */
-class SignumLogManager extends LogManager {
+public class SignumLogManager extends LogManager {
 
-  /**
-   * Logging reconfiguration in progress
-   */
-  private final AtomicBoolean loggingReconfiguration = new AtomicBoolean(false);
+    /**
+     * Logging reconfiguration in progress
+     */
+    private final AtomicBoolean loggingReconfiguration = new AtomicBoolean(false);
 
-  /**
-   * Create the Signum log manager
-   *
-   * We will let the Java LogManager create its shutdown hook so that the
-   * shutdown context will be set up properly.  However, we will intercept
-   * the reset() method so we can delay the actual shutdown until we are
-   * done terminating the Signum processes.
-   */
-  public SignumLogManager() {
-    super();
-  }
+    /**
+     * Create the Signum log manager
+     *
+     * We will let the Java LogManager create its shutdown hook so that the
+     * shutdown context will be set up properly. However, we will intercept
+     * the reset() method so we can delay the actual shutdown until we are
+     * done terminating the Signum processes.
+     */
+    public SignumLogManager() {
+        super();
+    }
 
-  /**
-   * Reconfigure logging support using a configuration file
-   *
-   * @param       inStream            Input stream
-   * @throws      IOException         Error reading input stream
-   * @throws      SecurityException   Caller does not have LoggingPermission("control")
-   */
-  @Override
-  public void readConfiguration(InputStream inStream) throws IOException {
-    loggingReconfiguration.set(true);
-    super.readConfiguration(inStream);
-    loggingReconfiguration.set(false);
-  }
+    /**
+     * Reconfigure logging support using a configuration file
+     *
+     * @param inStream Input stream
+     * @throws IOException       Error reading input stream
+     * @throws SecurityException Caller does not have LoggingPermission("control")
+     */
+    @Override
+    public void readConfiguration(InputStream inStream) throws IOException {
+        loggingReconfiguration.set(true);
+        super.readConfiguration(inStream);
+        loggingReconfiguration.set(false);
+    }
 
-  /**
-   * Reset the log handlers
-   *
-   * This method is called to reset the log handlers.  We will forward the
-   * call during logging reconfiguration but will ignore it otherwise.
-   * This allows us to continue to use logging facilities during Signum shutdown.
-   */
-  @Override
-  public void reset() {
-    if (loggingReconfiguration.get())
-      super.reset();
-  }
+    /**
+     * Reset the log handlers
+     *
+     * This method is called to reset the log handlers. We will forward the
+     * call during logging reconfiguration but will ignore it otherwise.
+     * This allows us to continue to use logging facilities during Signum shutdown.
+     */
+    @Override
+    public void reset() {
+        if (loggingReconfiguration.get())
+            super.reset();
+    }
 
-  /**
-   * Signum shutdown is now complete, so call LogManager.reset() to terminate
-   * the log handlers.
-   */
-  void signumShutdown() {
-    super.reset();
-  }
+    /**
+     * Signum shutdown is now complete, so call LogManager.reset() to terminate
+     * the log handlers.
+     */
+    void signumShutdown() {
+        super.reset();
+    }
 }
