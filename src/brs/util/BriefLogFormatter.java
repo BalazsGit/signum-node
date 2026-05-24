b81f32c814ada0ec98d9bee3ceb2fd2eced19b0e
BalazsGit <juhasz-nagyb@freemail.hu>
2026-05-24 16:31:56 +0200

    refactor: make BriefLogFormatter public for cross-package accessibility
    Updated the BriefLogFormatter class to support direct usage from other packages, specifically to enable the GUI console to use the same formatting logic as the standard log output.
    
    - Changed class visibility from package-private to public.
    - Updated the `init()` method to be public static.
    - Changed the constructor from private to public to allow instantiation in custom log handlers (e.g., in SignumGUI).
    - Refactored `init()` to retrieve handlers directly from the root logger.
    - Standardized indentation and code formatting throughout the file.

--- src/brs/util/BriefLogFormatter.java
+++ src/brs/util/BriefLogFormatter.java
@@ -13,56 +13,55 @@ import java.util.logging.Logger;
 /**
  * A Java logging formatter that writes more compact output than the default
  */
-class BriefLogFormatter extends Formatter {
+public class BriefLogFormatter extends Formatter {
 
-  /** Format used for log messages */
-  private static final ThreadLocal<MessageFormat> messageFormat = ThreadLocal.withInitial(() -> new MessageFormat("[{1}] {0,date,yyyy-MM-dd HH:mm:ss} {4} - {2}\n{3}"));
+    /** Format used for log messages */
+    private static final ThreadLocal<MessageFormat> messageFormat = ThreadLocal
+            .withInitial(() -> new MessageFormat("[{1}] {0,date,yyyy-MM-dd HH:mm:ss} {4} - {2}\n{3}"));
 
-  /** LoggerConfigurator instance at the top of the name tree */
-  private static final Logger logger = Logger.getLogger("");
+    /** singleton BriefLogFormatter instance */
+    private static final BriefLogFormatter briefLogFormatter = new BriefLogFormatter();
 
-  /** singleton BriefLogFormatter instance */
-  private static final BriefLogFormatter briefLogFormatter = new BriefLogFormatter();
-
-  /**
-   * Configures JDK logging to use this class for everything
-   */
-  static void init() {
-    Handler[] handlers = logger.getHandlers();
-    for (Handler handler : handlers)
-      handler.setFormatter(briefLogFormatter);
-  }
-
-  /**
-   * Format the log record as follows:
-   *
-   *     Date Level Message ExceptionTrace
-   *
-   * @param       logRecord       The log record
-   * @return                      The formatted string
-   */
-  @Override
-  public String format(LogRecord logRecord) {
-    Object[] arguments = new Object[5];
-    arguments[0] = new Date(logRecord.getMillis());
-    arguments[1] = logRecord.getLevel().getName();
-    arguments[2] = logRecord.getMessage();
-    arguments[4] = logRecord.getLoggerName();
-
-    Throwable exc = logRecord.getThrown();
-    if (exc != null) {
-      Writer result = new StringWriter();
-      exc.printStackTrace(new PrintWriter(result));
-      arguments[3] = result.toString();
-    } else {
-      arguments[3] = "";
+    /**
+     * Configures JDK logging to use this class for everything
+     */
+    public static void init() {
+        Handler[] handlers = Logger.getLogger("").getHandlers();
+        for (Handler handler : handlers)
+            handler.setFormatter(briefLogFormatter);
     }
 
-    arguments[4] = logRecord.getLoggerName();
+    /**
+     * Format the log record as follows:
+     *
+     * Date Level Message ExceptionTrace
+     *
+     * @param logRecord The log record
+     * @return The formatted string
+     */
+    @Override
+    public String format(LogRecord logRecord) {
+        Object[] arguments = new Object[5];
+        arguments[0] = new Date(logRecord.getMillis());
+        arguments[1] = logRecord.getLevel().getName();
+        arguments[2] = logRecord.getMessage();
+        arguments[4] = logRecord.getLoggerName();
 
-    return messageFormat.get().format(arguments);
-  }
+        Throwable exc = logRecord.getThrown();
+        if (exc != null) {
+            Writer result = new StringWriter();
+            exc.printStackTrace(new PrintWriter(result));
+            arguments[3] = result.toString();
+        } else {
+            arguments[3] = "";
+        }
 
-  private BriefLogFormatter() {}
+        arguments[4] = logRecord.getLoggerName();
+
+        return messageFormat.get().format(arguments);
+    }
+
+    public BriefLogFormatter() {
+    }
 
 }
